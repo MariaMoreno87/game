074723f15ce9a1c29515f28e86a627ff307e3da2
Maria Moreno <maria.ibarra87@gmail.com>
2019-02-13 14:20:07 -0800

    Updates

---
+++ precache-manifest.ec96c0e3be6b872a85886d710678ca8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffb2a4fcbf3ea7182df",
+    "url": "/game/static/js/runtime~main.dffb2a4f.js"
+  },
+  {
+    "revision": "5168b0ba54572cd23661",
+    "url": "/game/static/js/main.5168b0ba.chunk.js"
+  },
+  {
+    "revision": "7c6482712561ae3027d1",
+    "url": "/game/static/js/1.7c648271.chunk.js"
+  },
+  {
+    "revision": "5168b0ba54572cd23661",
+    "url": "/game/static/css/main.19bee71a.chunk.css"
+  },
+  {
+    "revision": "9afb116d29fa4c663819354a9b96cd19",
+    "url": "/game/index.html"
+  }
+];
